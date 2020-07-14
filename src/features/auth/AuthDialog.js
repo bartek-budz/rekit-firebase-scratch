bdeb74ae81204124aeaaaab00a2f792610fcd6c3
Bartek <bartlomiej.budz@gmail.com>
2020-07-14 21:04:41 +0200

    add logo to auth dialogs

--- src/features/auth/AuthDialog.js
+++ src/features/auth/AuthDialog.js
@@ -13,26 +13,33 @@ export default class AuthDialog extends Component {
   render() {    
     return (
       <div className="auth-auth-dialog">
-        <Container>
-          <Row>
-            {this.props.title &&
-            <Col xs={12} sm={9} className="auth-dialog-header">
-              <h2>{this.props.title}</h2>
-            </Col>
-            }
-            <Col xs={{span: 12}} sm={{ span: 3, offset: this.props.title ? 0 : 9 }} className="auth-dialog-header">
-              <LanguageButton />
-            </Col>
-          </Row>
-          <Row>
-            <Col className="auth-dialog-section">{this.props.form}</Col>
-          </Row>
-          {this.props.links &&
-          <Row>
-            <Col className="auth-dialog-section">{this.props.links}</Col>
-          </Row>          
-          }          
-        </Container>
+        <img src={require('../../images/rekit-react.png')} className="auth-dialog-logo" alt=""/>
+        <div className="auth-dialog-body">
+          <Container>
+            <Row>
+              {this.props.title && (
+                <Col xs={12} sm={9} className="auth-dialog-header">
+                  <h2>{this.props.title}</h2>
+                </Col>
+              )}
+              <Col
+                xs={{ span: 12 }}
+                sm={{ span: 3, offset: this.props.title ? 0 : 9 }}
+                className="auth-dialog-header"
+              >
+                <LanguageButton variant='secondary'/>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="auth-dialog-section">{this.props.form}</Col>
+            </Row>
+            {this.props.links && (
+              <Row>
+                <Col className="auth-dialog-section">{this.props.links}</Col>
+              </Row>
+            )}
+          </Container>
+        </div>
       </div>
     );
   }
